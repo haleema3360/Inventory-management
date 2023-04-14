4824545a280e4d39ee5ab265b036209188d133c9
haleema3360 <93393649+haleema3360@users.noreply.github.com>
2023-04-14 16:58:40 +0530

    modified

--- login_user.php
+++ login_user.php
@@ -5,7 +5,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     include 'dbconnect.php';
     $username = $_POST["username"];
     $password = $_POST["password"]; 
-    $sql = "Select * from `users` where username='$username'AND password='$password'";
+    $sql = "Select * from `users` where username='$username'";
     $result = mysqli_query($conn, $sql);
     $num = mysqli_num_rows($result);
     if ($num == 1){
