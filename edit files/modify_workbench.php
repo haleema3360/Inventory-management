3a57ba14b513c12884d404689ce766d0cb91c5de
Sarvanig16 <113225865+Sarvanig16@users.noreply.github.com>
2023-04-14 10:29:09 +0530

    Update modify_workbench.php

--- edit files/modify_workbench.php
+++ edit files/modify_workbench.php
@@ -8,7 +8,18 @@ if(isset($_POST['update'])){
         $duration=$_POST["duration"];
         
        $sql = "UPDATE `workbench` SET id='$id',department='$department',status='$status',duration='$duration' WHERE id='$id'";
-          
+        if($status=='Not Occupied'){
+           $sql1="SELECT product_id,product_name FROM products WHERE workbench_id=$id";
+           $stmt=$conn->query($sql1);
+           if($stmt!=false){
+               $row = $stmt->fetch_array(MYSQLI_ASSOC);
+               $pid=$row['product_id'];
+               $pname=$row['product_name'];
+               $sql2=mysqli_query($conn,"UPDATE porders SET status='WIP Inventory' WHERE product_id=$pid");
+               $sql3=mysqli_query($conn,"INSERT INTO `wip` (product_id,component,time_deposited) VALUES('$pid','$pname',CONVERT_TZ(NOW(),'+00:00','+05:30')) ");
+           }
+           
+       }
         $result = mysqli_query($conn, $sql);
           if($result){         
            header("location:admin_workbench.php");
